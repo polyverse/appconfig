8409156c323621e463e959f424f4c43fa2ee3d6e
chanaoka <chanaoka@concurix.com>
2015-10-15 00:00:07 -0700

    deleting from repo

--- generate_readme.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-go get github.com/robertkrimen/godocdown/godocdown
-$GOPATH/src/github.com/robertkrimen/godocdown/godocdown/godocdown github.com/polyverse-security/appconfig > README.md
-echo Done. README.md has been refreshed.
